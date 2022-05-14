b9a53431b70d68a17b0fa67ba41a6cddaa36afbe
Aaron Lambley <aaron.lambley@gmail.com>
2022-05-14 16:31:43 +0100

    added TODOs and methods to main amazon_book_finder file. Added some initial unit test. Added other folder structure to project. Added README.md

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Amazon Book Finder
 
 Amazon Book Finder is a Ruby file that finds amazon pages from a given list of ISBN-10 or ISBN-13 (in .csv or .txt files), retrieves current price and availability and stores information to an output file.
+
+## Future Developments
+
+Add methods to scrape ISBNs from another publishers website and export to book_files folder for amazon_book_finder to use
--- amazon_book_finder.rb
+++ amazon_book_finder.rb
@@ -1,4 +1,22 @@
+require "csv"
+require "open-uri"
+
 module AmazonBookFinder
   # main module
+  # Optional functionality - could scrape ISBNs from another site to feed to this module
+
+  def import_isbns
+    # TODO: look in book_files folder for .csv or .txt files
+    # TODO: check if isbns are 10 or 13 chars - if 13, pass to isbn 13 to 10 converter method
+    # TODO: archive import file to book_files/archive
+    # TODO: export isbn 10s to
+  end
+
+  def isbn_13_to_10(isbns)
+    # TODO: given an array of isbn 13s, map each
+  end
+  # TODO: import isbns from book_files folder
+  # TODO:
+  # TODO: export found data to output folder
 
 end
